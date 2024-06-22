6f73b757c638afb90a76c63176774a705b1fc6d3
HarshaDMadu <harshadhananjaya14@gmail.com>
2024-06-22 16:31:10 +0530

    Basic  Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import { Main } from "./Pages/main";
+import {Login} from "./Pages/login";
+import { Navbar } from './Components/navbar';
 
 function App() {
   return <div className='App'>
-    
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path = "/" element={ <Main /> } />
+          <Route path ="/login" element={<Login/>} />
+
+        </Routes>
+      </Router>
+
   </div>
     
 }
